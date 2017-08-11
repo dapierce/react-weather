cebd610a921103ba04cc5d37f6c4749ce84b5ac2
dapierce <pierceda@gmail.com>
2017-08-11 16:09:45 -0700

    Update to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,9 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is a basic weather app that polls the OpenWeatherMap API, made using React and following a Udemy tutorial.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a basic weather app that polls the <a href="http://openweathermap.org/apiOpenWeatherMap">OpenWeatherMap API</a>, made using <a href="https://facebook.github.io/react/React">React</a> and following a Udemy tutorial.</p>
+      <p>Get the source code at <a href="https://github.com/dapierce/react-weather">GitHub</a>!</p>
     </div>
   );
 }
